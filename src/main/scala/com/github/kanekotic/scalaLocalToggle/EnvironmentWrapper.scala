801d5d243cd20f5ea2f2ebb3d75dc04ca0c95cc5
kanekotic <alvarojosepl@gmail.com>
2017-11-06 08:59:46 +0100

    fix accesibility of Toggle Manager

--- src/main/scala/com/github/kanekotic/scalaLocalToggle/EnvironmentWrapper.scala
+++ src/main/scala/com/github/kanekotic/scalaLocalToggle/EnvironmentWrapper.scala
@@ -1,6 +1,6 @@
 package com.github.kanekotic.scalaLocalToggle
 
-private class EnvironmentWrapper {
+private[scalaLocalToggle] class EnvironmentWrapper {
   def get(name : String) : Option[String] = {
     sys.env.get(name)
   }
